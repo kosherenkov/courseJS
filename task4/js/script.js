dc1850b96a3a1361f35177154befa0f3c27972a3
Artem <a.kosherenkov@gmail.com>
2019-05-09 22:53:19 +0300

    Функція detectDayBadget

--- task4/js/script.js
+++ task4/js/script.js
@@ -37,10 +37,11 @@ function chooseExpenses () {
             alert("Поля не можуть бути порожніми");
             i--; 
         }
-    }
+    };
 }
 chooseExpenses();
 
+//Функція для підрахунку накопичень
 function checkSavings () {
     if (appData.savings == true) {
         let save = +prompt ("Яка у Вас сума накопичень?"),
@@ -107,6 +108,8 @@ do {
 }
 while ( i < 2);
 */
+
+//Функція для підрахунку бютжета на день
 function detectDayBudget() {
     appData.moneyPerDay = (appData.budget / 30).toFixed(1);
     alert("Б'ютжет на день: " + appData.moneyPerDay);
@@ -114,16 +117,21 @@ function detectDayBudget() {
  
 detectDayBudget();
 
-if (appData.moneyPerDay < 100) {
-    console.log("Мінімальний дохід");
-}   
-    else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000) {
-        console.log("Середній дохід");
-}   else if (appData.moneyPerDay > 1000) {
-        console.log("Високий дохід");
-}   else {
-    console.log("Вмнмкла помилка. ВВедіть коректні дані");
+//Функція для визначення рівня доходу
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Мінімальний дохід");
+    }   
+        else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000) {
+            console.log("Середній дохід");
+    }   else if (appData.moneyPerDay > 1000) {
+            console.log("Високий дохід");
+    }   else {
+        console.log("Вмнмкла помилка. ВВедіть коректні дані");
+    }
 }
+
+detectLevel();
 
 
 console.log(appData);

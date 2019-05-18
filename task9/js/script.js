f2d6f138079dcda8bcd5a661b5726f2c7c8efd88
Artem <a.kosherenkov@gmail.com>
2019-05-19 00:08:21 +0300

    Таймер

--- task9/js/script.js
+++ task9/js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function(){
 'use strict';
 
+// Tabs
 let tab = document.querySelectorAll(".info-header-tab"),
     info = document.querySelector(".info-header"),
     tabContent = document.querySelectorAll(".info-tabcontent");
@@ -32,6 +33,53 @@ let tab = document.querySelectorAll(".info-header-tab"),
             }
         }
     });
+
+    //Timer 
+    let deadline = "2019-05-20";
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            return {
+                "total" : t,
+                "hours" : hours,
+                "minutes" : minutes,
+                "seconds" : seconds
+            };   
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector(".hours"),
+            minutes = timer.querySelector(".minutes"),
+            seconds = timer.querySelector(".seconds"),
+            timeInterval = setInterval(upDateClock, 1000);
+
+        function upDateClock() {
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+            
+    }
+    
+    setClock("timer", deadline);
+
+
+
+
+
+
+
+
 
 
 });
